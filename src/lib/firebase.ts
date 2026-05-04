08f5d0eeb1acd466668f993494e60b4eedccaf73
DavidFranco135 <davidbhmg147@gmail.com>
2026-05-04 18:48:25 -0300

    Create firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBUvwY-e7h0KZyFJv7n0ignpzlMUGJIurU",
+  authDomain: "niklaus-b2b.firebaseapp.com",
+  projectId: "niklaus-b2b",
+  storageBucket: "niklaus-b2b.firebasestorage.app",
+  messagingSenderId: "936430517671",
+  appId: "1:936430517671:web:6a0f1b86a39621d74c4a82",
+  measurementId: "G-3VGKJGWFSY",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
